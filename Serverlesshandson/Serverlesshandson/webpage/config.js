c08a9be39431215cd6bbed80de6893319260c1ab
ikuma <tatsuya.ikuma@rolanddg.co.jp>
2020-11-13 15:11:39 +0900

    first

---
+++ Serverlesshandson/Serverlesshandson/webpage/config.js
@@ -0,0 +1,11 @@
+﻿// TODO: 作成したユーザープールおよびIDプールのIDに書き換えてください
+const CognitoConfig = {
+    region: 'us-east-1',
+
+    // User Pool
+    userPoolId: 'us-east-1_OyQENEzlv',
+    appClientId: '73r50u8kp8i0ha9olnf3t1lhan',
+
+    // Federated Identity
+    identityPoolId: 'us-east-1:0272fc42-5b9a-4d36-a6fd-a90ef7c3bbc5',
+}
